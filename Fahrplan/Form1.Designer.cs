8da2ea801998db79629b0713e624861fc6657ac0
nadineinglin <nadine.inglin@gmail.com>
2020-04-23 16:29:30 +0200

    Version1

--- Fahrplan/Form1.Designer.cs
+++ Fahrplan/Form1.Designer.cs
@@ -28,13 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonverbindungen = new System.Windows.Forms.Button();
+            this.textboxvon = new System.Windows.Forms.TextBox();
+            this.textboxnach = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.datetimepickerabfahrt = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // buttonverbindungen
+            // 
+            this.buttonverbindungen.Location = new System.Drawing.Point(46, 189);
+            this.buttonverbindungen.Name = "buttonverbindungen";
+            this.buttonverbindungen.Size = new System.Drawing.Size(381, 34);
+            this.buttonverbindungen.TabIndex = 0;
+            this.buttonverbindungen.Text = "Vebindungen suchen";
+            this.buttonverbindungen.UseVisualStyleBackColor = true;
+            this.buttonverbindungen.Click += new System.EventHandler(this.buttonverbindungen_Click);
+            // 
+            // textboxvon
+            // 
+            this.textboxvon.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textboxvon.Location = new System.Drawing.Point(46, 25);
+            this.textboxvon.Name = "textboxvon";
+            this.textboxvon.Size = new System.Drawing.Size(169, 20);
+            this.textboxvon.TabIndex = 2;
+            this.textboxvon.Text = "Von";
+            this.textboxvon.DoubleClick += new System.EventHandler(this.double_clickvon);
+            this.textboxvon.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textboxvon_KeyUp);
+            // 
+            // textboxnach
+            // 
+            this.textboxnach.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textboxnach.Location = new System.Drawing.Point(244, 25);
+            this.textboxnach.Name = "textboxnach";
+            this.textboxnach.Size = new System.Drawing.Size(183, 20);
+            this.textboxnach.TabIndex = 3;
+            this.textboxnach.Text = "Nach";
+            this.textboxnach.DoubleClick += new System.EventHandler(this.double_clicknach);
+            this.textboxnach.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textboxnach_KeyUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(46, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(244, 51);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(183, 95);
+            this.listBox2.TabIndex = 5;
+            // 
+            // datetimepickerabfahrt
+            // 
+            this.datetimepickerabfahrt.Location = new System.Drawing.Point(478, 25);
+            this.datetimepickerabfahrt.Name = "datetimepickerabfahrt";
+            this.datetimepickerabfahrt.Size = new System.Drawing.Size(117, 20);
+            this.datetimepickerabfahrt.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.datetimepickerabfahrt);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textboxnach);
+            this.Controls.Add(this.textboxvon);
+            this.Controls.Add(this.buttonverbindungen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonverbindungen;
+        private System.Windows.Forms.TextBox textboxvon;
+        private System.Windows.Forms.TextBox textboxnach;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.DateTimePicker datetimepickerabfahrt;
     }
 }
 
--- Fahrplan/Form1.cs
+++ Fahrplan/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,134 @@ namespace Fahrplan
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+            datetimepickerabfahrt.Format = DateTimePickerFormat.Custom;
+            datetimepickerabfahrt.CustomFormat = "dd/MM/yyyy hh:mm";
+            datetimepickerabfahrt.ShowUpDown = true;
+
+        }
+        SwissTransport.Transport transport = new SwissTransport.Transport();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void double_clicknach(object sender, EventArgs e)
+        {
+            bool click = false;
+            int i = 0;
+            while (i < 1)
+                if (!click)
+                {
+                    textboxnach = sender as TextBox;
+                    textboxvon = sender as TextBox;
+                    textboxnach.Text = String.Empty;
+                    textboxvon.Text = String.Empty;
+                    click = true;
+                    i++;
+                }
+
+        }
+
+        private void double_clickvon(object sender, EventArgs e)
+        {
+            bool click = false;
+            int i = 0;
+            while (i < 1)
+                if (!click)
+                {
+                    textboxnach = sender as TextBox;
+                    textboxvon = sender as TextBox;
+                    textboxnach.Text = String.Empty;
+                    textboxvon.Text = String.Empty;
+                    click = true;
+                    i++;
+                }
+        }
+
+        private void buttonverbindungen_Click(object sender, EventArgs e)
+        {
+
+            if (textboxvon.Text == string.Empty && textboxnach.Text == string.Empty)
+            {
+                MessageBox.Show("Es müssen Von und Nach Ort eingegeben werden");
+            }
+            else if (textboxvon.Text == string.Empty)
+            {
+                MessageBox.Show("Es müssen den Von Ort eingegeben werden");
+            }
+            else if (textboxnach.Text == string.Empty)
+            {
+                MessageBox.Show("Es muss der Nach Ort eingegeben werden");
+            }
+            else
+            {
+
+            }
+           
+
+            SwissTransport.Stations von = transport.GetStations(query: "Luzern");
+            SwissTransport.Stations nach = transport.GetStations(query: "Bern");
+
+        }
+
+
+
+        private void textboxvon_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+
+                listBox2.Items.Clear();
+                Stations resultat = transport.GetStations(textboxvon.Text);
+                listBox1.Items.Clear();
+
+                foreach (Station station in resultat.StationList)
+                {
+                    listBox1.Items.Add(station.Name);
+                }
+            }
+
+
+            catch
+            {
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void textboxnach_KeyUp(object sender, KeyEventArgs e)
+        {
+            listBox2.Items.Clear();
+            Stations resultat = transport.GetStations(textboxnach.Text);
+            listBox2.Items.Clear();
+
+            foreach (Station station in resultat.StationList)
+            {
+                listBox2.Items.Add(station.Name);
+            }
+
         }
     }
 }
